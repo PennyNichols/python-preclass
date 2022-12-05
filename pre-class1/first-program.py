1b965b0117a5f9364ab56386914f69de45a5388a
pnichols789 <pnichols789@gmail.com>
2022-12-05 10:26:17 -0600

    -

--- pre-class1/first-program.py
+++ pre-class1/first-program.py
@@ -1,1 +1,12 @@
 print('being a good person')
+print('')
+print("Clarusway will change my life")
+# print('We should have enough time for our family")
+example1 = 'sometimes what you say is less important than how you say it'
+print(type(example1))
+a = 36.5
+b = '30'
+c = '3.5'
+d = ' F is enough for room temperature.'
+
+print(str(a+int(b)+float(c))+d)
